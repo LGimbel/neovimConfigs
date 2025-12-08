8deb9130521da99abf47b89c62336a34357b6234
Liam J Gimbel <ljgimbel@captechu.edu>
2025-12-08 12:31:19 -0500

    first commit

---
+++ init.lua
@@ -0,0 +1,102 @@
+-- Set leader key to 'h'
+vim.g.mapleader = 'h'
+
+-- Enable line numbers and relative line numbers (vim.smartRelativeLine: true)
+vim.opt.number = true
+vim.opt.relativenumber = false
+
+-- Enable showing commands on the status bar (vim.otherSettings: { "showcmd": true })
+vim.opt.showcmd = true
+
+
+local function set_keymaps()
+
+      vim.opt.signcolumn = "yes"
+  -- arrow keys no suck
+      vim.keymap.set({'n', 'v', 'i'}, '<Up>', '<Up>', { noremap = true, silent = true })
+      vim.keymap.set({'n', 'v', 'i'}, '<Down>', '<Down>', { noremap = true, silent = true })
+      vim.keymap.set({'n', 'v', 'i'}, '<Left>', '<Left>', { noremap = true, silent = true })
+      vim.keymap.set({'n', 'v', 'i'}, '<Right>', '<Right>', { noremap = true, silent = true })
+    -- index end and home
+      vim.keymap.set('o', 'U', '^', { remap = true, desc = 'Move to start of line' })
+      vim.keymap.set('o', 'H', '$', { remap = true, desc = 'Move to end of line' })
+
+    -- P past last yank not last delete
+    vim.keymap.set('n', 'P', '"0p', { noremap = true, desc = 'Paste from clipboard register' })
+
+    -- Leader m to :marks (List Marks)
+    vim.keymap.set('n', '<leader>m', ':marks<CR>', { noremap = true, desc = 'List Marks' })
+
+    -- Mark/Jump Mappings
+    vim.keymap.set('n', 'l', "'", { noremap = true, desc = 'Jump to last cursor position' })
+    vim.keymap.set('n', 'le', "'.", { noremap = true, desc = 'Jump to last change' })
+    vim.keymap.set('n', 'lr', "''", { noremap = true, desc = 'Jump to beginning of line of last position' })
+    vim.keymap.set('n', 'lj', ':ju<CR>', { noremap = true, desc = 'Jump to latest location' })
+    vim.keymap.set('n', 'lda', ':delm!<CR>', { noremap = true, desc = 'Delete All Marks' })
+
+    -- 'b' to '%' (Match parenthesis/brace)
+    vim.keymap.set('n', 'b', '%', { remap = true, desc = 'Match parenthesis' })
+
+    -- hop bindings
+    local hop = require('hop')
+    local directions = require('hop.hint').HintDirection
+
+    vim.keymap.set('n', '<leader>hw', function() hop.hint_words() end, { desc = 'Hop to Word' })
+    vim.keymap.set('n', '<leader>hl', function() hop.hint_lines_skip_whitespace() end, { desc = 'Hop to Line' })
+    vim.keymap.set('n', '<leader>hc', function() hop.hint_char2() end, { desc = 'Hop to Character' })
+    vim.keymap.set('n', '<leader>/', function() hop.hint_patterns() end, { desc = 'Hop to Pattern' })
+    vim.keymap.set('n', '<leader>f', function() hop.hint_char1({current_line_only = true}) end, { desc = 'Hop to Character' })
+
+    vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>", { silent = true, noremap = true, desc = "Clear Search Highlight" })
+
+
+    -- Load the VSCode API if running inside the extension
+    local is_vscode = vim.g.vscode
+    if is_vscode then
+        local vscode = require('vscode')
+
+        -- Leader key mappings for VS Code commands
+        vim.keymap.set('n', '<leader>p', function() vscode.action('workbench.action.quickOpen') end, { silent = true, desc = 'Quick Open' })
+        vim.keymap.set('n', '<leader>b', function() vscode.action('workbench.action.showAllEditors') end, { silent = true, desc = 'Show All Editors/Buffers' })
+        vim.keymap.set('n', '<leader>t', function() vscode.action('workbench.action.terminal.toggleTerminal') end, { silent = true, desc = 'Toggle Terminal' })
+        vim.keymap.set('n', '<leader>e', function() vscode.action('editor.action.marker.next') end, { silent = true, desc = 'Next Error/Marker' })
+        vim.keymap.set('n', '<leader>g', function() vscode.action('editor.action.revealDefinition') end, { desc = 'Go to Definition' })
+        vim.keymap.set('n', '<leader>hf', function() vscode.action('editor.action.formatDocument') end, { desc = 'Format Document' })
+        vim.keymap.set('n', '<leader>l', function() vscode.action('workbench.action.toggleSidebarVisibility') end, { desc = 'Toggle Sidebar' })
+        vim.keymap.set('n', '<leader>d', function() vscode.action('workbench.action.nextEditor') end, { desc = 'Next Tab' })
+        vim.keymap.set('n', '<leader>D', function() vscode.action('workbench.action.previousEditor') end, { desc = 'Previous Tab' })
+        vim.keymap.set('n', '<leader>i', function() vscode.action('editor.action.showHover') end, { desc = 'Show Hover' })
+        vim.keymap.set('n', '<leader>w', function() vscode.action('workbench.action.closeActiveEditor') end, { desc = 'Close Active Editor' })
+        vim.keymap.set('n', '<leader>s', function() vscode.action('workbench.action.findInFiles') end, { desc = 'Search in Files' })
+        vim.keymap.set('n', '<leader>r', function() vscode.action('references-view.findReferences') end, { desc = 'Find References' })
+      -- clear window
+        vim.keymap.set('n', '<space>c', function()
+           vscode.action('workbench.action.closeAuxiliaryBar')
+           vscode.action('workbench.action.closeSidebar')
+         end, { desc = 'Close all extra windows' })
+    end
+end
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Initialize lazy.nvim and load the plugins
+require("lazy").setup("plugins", {
+    -- Automatically install missing plugins on startup
+    checker = { enabled = true, notify = false },
+    -- Automatically check for plugin updates
+    change_detection = {
+        enabled = true,
+        notify = false,
+    },
+})
+set_keymaps()
---
+++ lua/plugins.lua
@@ -0,0 +1,61 @@
+return {
+    {
+    'smoka7/hop.nvim',
+    version = "*",
+    opts = {
+        keys = 'etovxqpdygfblzhckisuran'
+    }
+},
+    {
+        'machakann/vim-highlightedyank',
+        config = function()
+            vim.g.highlightedyank_enable_highlight = 1
+            vim.g.highlightedyank_highlight_duration = 300
+        end
+    }, 
+    {
+        'kylechui/nvim-surround',
+        version = "*",
+        event = "VeryLazy",
+        config = function()
+            require('nvim-surround').setup({})
+        end
+    },
+    {
+    'tpope/vim-commentary',
+    event = 'VeryLazy',
+    },
+     {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = { "go", "lua", "python", "rust", "html"},
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true,
+            goto_next_start = {
+              ["]m"] = "@function.outer",
+              ["]]"] = "@class.outer",
+            },
+            goto_previous_start = {
+              ["[m"] = "@function.outer",
+              ["[["] = "@class.outer",
+            },
+          },
+        },
+      })
+    end,
+  },
+}
