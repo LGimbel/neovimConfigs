846c610593d2335d0cbca3c34b6357ebb773468b
Liam J Gimbel <ljgimbel@captechu.edu>
2026-01-27 01:26:19 -0500

    fix treesitter breakage

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,61 +1,67 @@
 return {
-    {
+  {
     'smoka7/hop.nvim',
     version = "*",
     opts = {
-        keys = 'etovxqpdygfblzhckisuran'
+      keys = 'etovxqpdygfblzhckisuran'
     }
-},
-    {
-        'machakann/vim-highlightedyank',
-        config = function()
-            vim.g.highlightedyank_enable_highlight = 1
-            vim.g.highlightedyank_highlight_duration = 300
-        end
-    }, 
-    {
-        'kylechui/nvim-surround',
-        version = "*",
-        event = "VeryLazy",
-        config = function()
-            require('nvim-surround').setup({})
-        end
-    },
-    {
+  },
+  {
+    'machakann/vim-highlightedyank',
+    config = function()
+      vim.g.highlightedyank_enable_highlight = 1
+      vim.g.highlightedyank_highlight_duration = 300
+    end
+  },
+  {
+    'kylechui/nvim-surround',
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+      require('nvim-surround').setup({})
+    end
+  },
+  {
     'tpope/vim-commentary',
     event = 'VeryLazy',
-    },
-     {
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    commit = 'v0.9.2',
+    build = ':TSUpdate',
+  },
+  {
     'nvim-treesitter/nvim-treesitter-textobjects',
+    commit = 'v0.0.2',
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
     config = function()
       require('nvim-treesitter.configs').setup({
-        ensure_installed = { "go", "lua", "python", "rust", "html"},
+        ensure_installed = { 'go', 'lua', 'python', 'rust', 'html' },
         textobjects = {
           select = {
             enable = true,
             lookahead = true,
             keymaps = {
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
+              ['af'] = '@function.outer',
+              ['if'] = '@function.inner',
+              ['ac'] = '@class.outer',
+              ['ic'] = '@class.inner',
             },
           },
           move = {
             enable = true,
             set_jumps = true,
             goto_next_start = {
-              ["<leader><Down>"] = "@function.outer",
-              ["]]"] = "@class.outer",
+              ['<leader><Down>'] = '@function.outer',
+              [']]'] = '@class.outer',
             },
             goto_previous_start = {
-              ["<leader><Up>"] = "@function.outer",
-              ["[["] = "@class.outer",
+              ['<leader><Up>'] = '@function.outer',
+              ['[['] = '@class.outer',
             },
           },
         },
       })
-    end,
+    end
   },
 }
